5069717e146ec85bf7637808a99ea8a01da32316
anais meirone <anais.meirone@live.fr>
2022-01-12 14:23:20 +0100

    add link_to a propos

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,27 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
+   def new
+    @project = Project.new
+  end
+
+  # def create
+  #   @project = Project.new(project_params)
+
+  #   if @project.save
+  #     redirect_to @project, notice: 'project was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+
+private
+
+def project_params
+    params.require(:project).permit(:title, :description)
+  end
 end
